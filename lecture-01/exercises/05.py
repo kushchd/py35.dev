37fb3c4de1d193e80d90fa4281e04dc54702e8e6
kushchd <kushchd2013@gmail.com>
2024-11-16 01:36:40 +0200

    task 5

---
+++ lecture-01/exercises/05.py
@@ -0,0 +1,5 @@
+#
+Distance = int(input("Input distance in feet: "))
+print("The distance in inches is: ", Distance * 12, "inches")
+print("The distance in yards is: ", Distance * 0.333333333, "yards")
+print("The distance in miles is: ", Distance * (0.000189393939), "miles")
