7a91d097ee6f54073dea411ae0820fde24790c3d
kushchd <kushchd2013@gmail.com>
2024-11-16 09:56:40 +0200

    task 8

---
+++ lecture-01/exercises/08.py
@@ -0,0 +1,8 @@
+#
+
+a = int(input("Enter days: "))
+b = int(input("Enter hours: "))
+c = int(input("Enter minutes: "))
+d = int(input("Enter seconds: "))
+
+print("The amounts of seconds: ", (a * 86400) + (b * 3600) + (c * 60) + d )
