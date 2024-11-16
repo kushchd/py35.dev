0ae4a6d5d4beb17e4d68c1608d813188c5c3b2e1
kushchd <kushchd2013@gmail.com>
2024-11-16 13:12:50 +0200

    task

---
+++ lecture-01/exercises/15.py
@@ -0,0 +1,8 @@
+#
+a = int(input("Enter speed of the car: "))
+b = (6)
+c = (10)
+d = (15)
+print("Distance for 6 hours: ", b * a)
+print("Distance for 10 hours: ", c * a)
+print("Distance for 6 hours: ", d * a)
