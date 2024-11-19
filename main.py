68477db887851ad04b975124c095fcb3c093e779
kushchd <kushchd2013@gmail.com>
2024-11-19 21:49:32 +0200

    t

--- main.py
+++ main.py
@@ -19,3 +19,29 @@ x3 = [1,2,3]
 y3 = [1,2,3]
 print(x3 is not y3)
 
+var = 1
+print(type (var))
+print(id (var))
+print(2 ** 200)
+
+print("""
+Hello
+Word
+""")
+print("Hello      Word")
+print("_" * 80)
+
+name = "Jon"
+last_name = "Don"
+print(f"My name {name} {last_name}")
+
+def hell(p):
+    print(f"Hello {p}")
+
+hell("Word")
+
+hello = "Hello Word"
+for c in hello:
+    print(c)
+
+    
