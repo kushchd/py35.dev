4902bcf7cc008d28c26a5a16a43309be3074bf3f
kushchd <kushchd2013@gmail.com>
2024-11-19 19:38:12 +0200

    t1

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ print(True and True)
 print(True and False)
 print(True or False)
 print(not True)  
+
+a="Kira"
+print(a)
+
+a = 1
+b = 1
+print (a is b)
+
+x3 = [1,2,3]
+y3 = [1,2,3]
+print(x3 is not y3)
+
