0b3e8c9b2bbc943a2c2dfced6419a3953c1cd898
kushchd <kushchd2013@gmail.com>
2024-11-20 16:09:33 +0200

    t2.4

--- lecture-02/exercises/04.py
+++ lecture-02/exercises/04.py
@@ -5,7 +5,7 @@ case1 = str1[middle_symbol-1:middle_symbol+2]
 print(f"Вихідні дані case 1: {case1}")
 
 
-str1 = "JaSonAy"
-middle_symbol = len(str1) // 2
+str2 = "JaSonAy"
+middle_symbol = len(str2) // 2
 case2 = str1[middle_symbol-1:middle_symbol+2]
 print(f"Вихідні дані case 2: {case2}")
